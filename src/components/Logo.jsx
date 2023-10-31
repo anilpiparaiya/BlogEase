c8a443b6548e7393a6863aa4964272f2badeb118
Anil Kumar Piparaiya <anilkumarpiparaiya@gmail.com>
2023-10-31 14:52:35 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    // <div>Logo</div>
+    <div>
+      <h2><span>Blog</span>Ease</h2>
+    </div>
+  )
+}
+
+export default Logo
